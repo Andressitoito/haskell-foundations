2f677fbfb9ea8e625f6a82f83a34d8acbe775da1
Andresito <60946504+Andressitoito@users.noreply.github.com>
2024-10-01 20:30:26 -0300

    01-add-two-numbers
    -- Problem Statement:
    -- Write a function that takes two integers and returns their sum.
    
    -- Focus:
    -- Tests your understanding of function definition and applying inputs.

---
+++ algorithms/01-add-two-numbers.hs
@@ -0,0 +1,12 @@
+-- Problem Statement:
+-- Write a function that takes two integers and returns their sum.
+
+-- Focus:
+-- Tests your understanding of function definition and applying inputs.
+
+addTwoNumbers :: Int -> Int -> Int
+addTwoNumbers x y = x + y
+
+main :: IO ()
+main do 
+ print(addTwoNumbers 8 7)
